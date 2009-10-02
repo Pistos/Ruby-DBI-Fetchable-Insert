f009a28ab17513b37307477513b881e1fc4a7ae5
Pistos <gitsomegrace.5.pistos@geoshell.com>
2009-10-01 21:11:02 -0400

    Use setup and teardown for tests.

--- test.rb
+++ test.rb
@@ -3,22 +3,28 @@ require 'test/unit'
 
 class DBITest < Test::Unit::TestCase
 
+  def setup
+    @dbh = DBI.connect( 'DBI:Pg:dbi_f_i', 'test' )
+    assert_not_nil @dbh
+    assert @dbh.connected?
+  end
+
+  def teardown
+    @dbh.disconnect
+  end
+
   def test_fetchable_insert
-    dbh = DBI.connect( 'DBI:Pg:dbi_f_i', 'test' )
-    assert_not_nil dbh
-    assert dbh.connected?
-
-    dbh.do "DELETE FROM t"
+    @dbh.do "DELETE FROM t"
 
     inserted = dbh.do "INSERT INTO t ( data ) VALUES ( 'foo' );"
     assert( inserted > 0 )
-    inserted = dbh.do "INSERT INTO t ( data ) VALUES ( 'foo' );"
+    inserted = @dbh.do "INSERT INTO t ( data ) VALUES ( 'foo' );"
     assert( inserted > 0 )
 
-    rows = dbh.select_all "SELECT * FROM t"
+    rows = @dbh.select_all "SELECT * FROM t"
     assert_equal( 2, rows.size )
 
-    rows = dbh.select_all "INSERT INTO api__sub1 ( data ) VALUES ( 'foo' )"
+    rows = @dbh.select_all "INSERT INTO api__sub1 ( data ) VALUES ( 'foo' )"
     assert_equal( 3, rows.size )
   end
 
